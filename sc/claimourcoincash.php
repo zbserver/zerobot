241431cdeac99033ddb7985c31576bbb7381c5d0
zbserver <novandrawahyuherwanto@gmail.com>
2025-03-16 22:09:19 +0700

    Update claimourcoincash.php

--- sc/claimourcoincash.php
+++ sc/claimourcoincash.php
@@ -4,6 +4,7 @@ define('version','1.0.0');
 define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
+if((!is_dir(Data.host[0])){unlink(Data.host[0]."/cookie.txt");}
 if(!is_dir(Data.host[0])){system("mkdir ".Data.host[0]);}
 Function h(){
     $h[] = "Host: ".host[1];
