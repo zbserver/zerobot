adc56fbb9bf40d03211e6015b2ae13089ffb8058
zbserver <novandrawahyuherwanto@gmail.com>
2025-03-19 12:34:03 +0700

    Update ourcoincash.php

--- sc/ourcoincash.php
+++ sc/ourcoincash.php
@@ -1,14 +1,25 @@
 <?php
 define('host',['Ourcoincash','ourcoincash.xyz','']);
-define('version','1.0.0');
+define('version','1.0.1');
 define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
 Del_Cok();
 Function h(){
     $h[] = "Host: ".host[1];
-    $h[] = "cookie: ".file_get_contents(Data.cok);
+    $h[] = "accept-language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7";
     $h[] = "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
+    $h[] = "cookie: ".file_get_contents(Data.cok);
+    $h[] = 'sec-ch-ua: "Not A(Brand";v="8", "Chromium";v="132"';
+    $h[] = 'sec-ch-ua-arch: ""';
+    $h[] = 'sec-ch-ua-bitness: ""';
+    $h[] = 'sec-ch-ua-full-version: "132.0.6961.0"';
+    $h[] = 'sec-ch-ua-full-version-list: "Not A(Brand";v="8.0.0.0", "Chromium";v="132.0.6961.0"';
+    $h[] = 'sec-ch-ua-mobile: ?1';
+    $h[] = 'sec-ch-ua-model: "SM-A057F"';
+    $h[] = 'sec-ch-ua-platform: "Android"';
+    $h[] = 'sec-fetch-site: same-origin';
+    $h[] = 'upgrade-insecure-requests: 1';
     $h[] = "user-agent: ".file_get_contents(Data.uag);
     return $h;
 }
@@ -16,7 +27,7 @@ Function balance(){
     $r = get(web."/dashboard");
     $b = Ambil($r,'<p class="acc-amount"><i class="fas fa-coins"></i> ','</p>',1);
     $e = Ambil($r,'<p class="text-warning"><i class="fas fa-bolt"></i> ','</p>',1);
-    return ["b"=>$b.h.' Coins',"e"=>$e];
+    return ["b"=>$b,"e"=>$e];
 }
 Menu_Api();
 Awal:
@@ -25,13 +36,14 @@ ban();
 $r = get(web."/dashboard");
 $lg = Ambil($r,'<span>','</span>',2);
 if(!$lg){print Error("Cookie Experied").n;Del();die;}
-else{print " ".p."Login success.";sleep(2);print r;}
-$r = balance();
-print pesan(0,cpm[2]).p."Balance".panah.p.$r["b"].n;
-print pesan(0,cpm[2]).p."Energy ".panah.p.$r["e"].n;
-print pesan(0,cpm[2]).p."Apikey ".panah.p.Api_Bal().n;
-print " ".line();
-
+else{print " ".p."Login success.";sleep(2);print rr;}
+$r = Balance();
+echo n;
+echo line_at();
+echo line_tg().msg(1,"Balance").panah.p.$r["b"].n;
+echo line_tg().msg(1,"Energy ").panah.p.$r["e"].n;
+echo line_tg().msg(1,"Apikey ").panah.p.Api_Bal().n;
+echo line_bw();
 Faucet:
 while(true){
     $r = get(web."/faucet");
@@ -51,6 +63,9 @@ while(true){
         $data="g-recaptcha-response=$cap&captchaType=hcaptcha&csrf_token_name=$c_t";
         post(web."/firewall/verify",$data);
     }
+    if(preg_match("/Just a moment.../",$r)){
+        echo msg(4,"Cloudflare!").n;del();Del_Cok();die;
+    }
     $atb = anti_bot($r);
     if(!$atb)continue;
     $c_t = Ambil($r,'name="csrf_token_name" id="token" value="','">',1);
@@ -61,9 +76,14 @@ while(true){
         if(preg_match("/title: 'Good job!'/",$post)){
             $r = balance(); 
             $hasil = Ambil($post,"text: '","has been added to your balance",1);
-            Efek(success($hasil).n.bal($r["b"]).n.cekapi().n,5000);
-            print " ".p.line();
-            tim(5);
+            echo n;
+            echo line_at();
+            echo line_tg().msg(1,"Reward ").panah.p.$hasil.n;
+            echo line_tg().msg(2,"Balance").panah.p.$r["b"].n;
+            echo line_tg().msg(3,"Energy ").panah.p.$r["e"].n;
+            echo line_tg().msg(3,"Apikey ").panah.p.Api_Bal().n;
+            echo line_bw();
+            tim(20);
         }
     }
 }
