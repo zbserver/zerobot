837a5d57aa415c7e13b917d4f39e62ce168fc20e
zbserver <novandrawahyuherwanto@gmail.com>
2025-03-22 12:24:15 +0700

    Create autofaucettop.php

---
+++ sc/autofaucettop.php
@@ -0,0 +1,97 @@
+<?php
+error_reporting(0);
+define('host',['Autofaucet','autofaucet.top','']);
+define('version','1.0.0');
+define('cok','cookie.'.host[0]);
+define('uag','user_agent');
+define('web','https://'.host[1]);
+Del_Cok();
+Function h(){
+    $h[] = "Host: ".host[1];
+    $h[] = "accept-language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7";
+    $h[] = "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
+    $h[] = "cookie: ".file_get_contents(Data.cok);
+    $h[] = "user-agent: ".file_get_contents(Data.uag);
+    return $h;
+}
+Function dashboard(){
+    $r=get(web."/app/dashboard");
+    if(preg_match("/Dashboard | Autofaucet/",$r)){
+        echo p."  login success";sleep(1);echo rr;
+    }else{
+        echo msg(4,"login failed");Del_Cok();Del();die;
+    }  
+}
+savecokua();
+cl();
+ban();
+dashboard();
+Menu_Coin:
+echo line_at();
+echo line_tg().p.str_pad("Menu Coin", 20, " ", STR_PAD_BOTH).n;
+echo line_bw();
+echo NoLi(1,"LTC").n;
+echo NoLi(2,"USDT").n;
+echo NoLi(3,"DOGE").n;
+echo NoLi(4,"DGB").n;
+echo n;
+$pilih = readline(w3." Input".panah.p);
+if($pilih == 1){
+    cl(); ban();
+    while(true){claim("LTC");}
+}elseif($pilih == 2){
+    cl(); ban();
+    while(true){claim("USDT");}
+}elseif($pilih == 3){
+    cl(); ban();
+    while(true){claim("DOGE");}
+}elseif($pilih == 4){
+    cl(); ban();
+    while(true){claim("DGB");}
+}else{
+    echo msg(4,"Bad number");sleep(1);echo rr; goto Menu_Coin;
+}
+Function Claim($coin){
+    a:
+    $r = get(web."/app/faucet?currency=$coin");
+    if(preg_match('/Shortlink in order to claim from the faucet!/',$r)){
+        $err = Ambil($r,"html: '","'",1);
+		echo msg(4,$err).n;
+		exit;
+		}
+		
+    $ictok = Ambil($r,"name='_iconcaptcha-token' value='","'",1);
+    $icon = iconBypass($ictok);
+    if(!$icon){
+        echo rr;
+        echo msg(4,"Bypass Failed");
+        sleep(1);
+		echo rr;
+		goto a;
+	}
+	echo bps_cap();sleep(1);
+	echo rr;
+	$data = [];
+	$data = array_merge($data, $icon);
+	$data = http_build_query($data);
+	$r = postt(web."/app/faucet/verify?currency=".$coin,h(), $data);
+
+	$has = Ambil($r,"title: '","',",1);
+	if($has == "Great!"){
+	    $rd = Ambil($r,"text: '"," account'",1);
+	    echo line_at();
+	    echo line_tg().msg(1,$rd).n;
+	    echo line_bw();
+	    tim(10);
+	    goto a;
+	}
+	if(preg_match("/1 Shortlink must be/",$r)){
+	    echo msg(4,"1 Shortlink must be completed to continue again!").n;die;
+	}
+    if(preg_match("/sufficient found/",$r)){
+        Echo msg(4,"Sufficient Found").n;die;
+    }
+}
+while(true){
+    claim($coin);
+}
