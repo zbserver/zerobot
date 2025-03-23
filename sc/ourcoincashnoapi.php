4e2a3c5b51f826fab11be37a3499076f9ac7a242
zbserver <novandrawahyuherwanto@gmail.com>
2025-03-24 04:09:21 +0700

    Update ourcoincashnoapi.php

--- sc/ourcoincashnoapi.php
+++ sc/ourcoincashnoapi.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 define('host',['OurcoincashFREE','ourcoincash.xyz','']);
-define('version','1.0.0');
+define('version','1.0.1');
 define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
@@ -28,7 +28,6 @@ if(!$lg){print Error("Cookie Experied").n;Del();die;}
 else{echo " ".p." Login success."; echo r;sleep(2);}
 $r = balance();
 echo rr;
-echo n;
 echo line_at();
 echo line_tg().msg(2,"Balance").panah.p.$r["b"].n;
 echo line_tg().msg(3,"Energy ").panah.p.$r["e"].n;
@@ -42,30 +41,33 @@ while(true){
     if(preg_match("/Daily limit reached/",$r)){
         echo Error("Daily limit reached").n;die;
     }
-    $atb = atb_3($r);
-    if(!$atb){echo rr;continue;}
-    
     $c_t = Ambil($r,'name="csrf_token_name" id="token" value="','">',1);
     $tok = Ambil($r,'name="token" value="','">',1);
-    $data ="$atb&csrf_token_name=$c_t&token=$tok";
+    if($atb = atb_3($r)){
+        $data ="$atb&csrf_token_name=$c_t&token=$tok";}
+        goto exekusi;
+    if(!$atb){
+        $data ="csrf_token_name=$c_t&token=$tok";
+    }
+    exekusi:
     $post = post(web."/faucet/verify",$data);
-    if($post){
-        if(preg_match("/Invalid Anti-Bot Links/",$post)){
-            echo Error("Invalid Antibot! ").p."[".k.$a = $a + 1 .p."]";sleep(2);echo r;goto en;
+    if(preg_match("/Just a moment/",$post)){
+        echo msg(4,"Cloudflare!").n;sleep(1);del();del_cok();die;
+    }
+    
+    if(preg_match("/Invalid Anti-Bot Links/",$post)){
+        echo Error("Invalid Antibot! ").p."[".k.$a = $a + 1 .p."]";sleep(2);echo r;goto en;
         }
-        if(preg_match("/title: 'Good job!'/",$post)){
-            bps_anbot();
-            $r = balance(); 
-            $hasil = Ambil($post,"text: '"," coins has been added to your balance",1);
-            echo n;
-            echo line_at();
-            echo line_tg().msg(1,"Reward ").panah.p.$hasil.n;
-            echo line_tg().msg(2,"Balance").panah.p.$r["b"].n;
-            echo line_tg().msg(3,"Energy ").panah.p.$r["e"].n;
-            echo line_bw();
-            tim(10);
-            $a = 0;
-        }
+    if(preg_match("/title: 'Good job!'/",$post)){
+        $r = balance(); 
+        $hasil = Ambil($post,"text: '"," coins has been added to your balance",1);
+        echo line_at();
+        echo line_tg().msg(1,"Reward ").panah.p.$hasil.n;
+        echo line_tg().msg(2,"Balance").panah.p.$r["b"].n;
+        echo line_tg().msg(3,"Energy ").panah.p.$r["e"].n;
+        echo line_bw();
+        tim(10);
+        $a = 0;
     }
     en:
 }
