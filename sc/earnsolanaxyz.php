1950529d2b08f762242e06e6493e837868ebca50
zbserver <novandrawahyuherwanto@gmail.com>
2025-04-12 10:20:00 +0700

    Update earnsolanaxyz.php

--- sc/earnsolanaxyz.php
+++ sc/earnsolanaxyz.php
@@ -5,7 +5,7 @@ define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
 Del_Cok();
-Function h($data = 0){
+Function h(){
     $h[] = "Host: ".host[1];
     $h[] = "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
     $h[] = "accept-language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7";
