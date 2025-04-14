09067ee262ac88480801859f92baaae365c5a8d0
zbserver <novandrawahyuherwanto@gmail.com>
2025-04-14 10:07:51 +0700

    PATI, 14 APRIL 2025

--- sc/cryptofuture.php
+++ sc/cryptofuture.php
@@ -1,6 +1,6 @@
 <?php
 define('host',['Cryptofuture','cryptofuture.co.in','']);
-define('version','1.0.0');
+define('version','1.0.1');
 define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
@@ -8,11 +8,10 @@ define('web','https://'.host[1]);
 Del_Cok();
 Function h($data = 0){
     $h[] = "Host: ".host[1];
-    if($data)$h[] = "Content-Length: ".strlen($data);
-    $h[] = "cookie: ".file_get_contents(Data.cok);
     $h[] = "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
     $h[] = "accept-language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7";
     $h[] = "user-agent: ".file_get_contents(Data.uag);
+    $h[] = "cookie: ".file_get_contents(Data.cok);
     return $h;
 }
 Function dashboard(){
